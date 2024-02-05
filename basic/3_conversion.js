f8d53dfdf327a3c51133d4a9e1b3f20e579a1e82
vinod yadav <139549615+vinod26011995@users.noreply.github.com>
2024-02-05 10:09:54 +0000

    conversion

---
+++ basic/3_conversion.js
@@ -0,0 +1,19 @@
+// let score="33";
+// console.log(typeof score);
+
+// let valueNumber=Number(score);
+//outputs
+//'33'= Number
+//'33ab'=NaN
+//'null'-NaN
+//boolean True/false=number
+
+
+
+// console.log( typeof valueNumber);
+
+//now boolean
+let Newbolean=1;
+let boolean=Boolean(Newbolean);
+console.log(boolean);
+
